c00bc08a8a26ca5f8ecc3c8b17678ded516455b5
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-10-02 12:10:49 +0530

    New Patterns

---
+++ pattern-12.cpp
@@ -0,0 +1,28 @@
+//                    10
+//                  9 10
+//                8 9 10 
+//              7 8 9 10
+//            6 7 8 9 10
+//          5 6 7 8 9 10
+//        4 5 6 7 8 9 10
+//      3 4 5 6 7 8 9 10
+//    2 3 4 5 6 7 8 9 10
+//  1 2 3 4 5 6 7 8 9 10
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int i, j;
+    for (i = 1; i <= 10; i++){
+        for(j = 1; j <= 10; j++){
+            
+            if ( j>=11-i  )
+               
+                cout << j ;
+            else
+                cout << " ";    
+        }
+        cout << endl;
+    }
+    return 0;
+}
